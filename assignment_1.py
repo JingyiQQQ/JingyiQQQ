d2bd3aeb530ebeeb76352f8dd26a0ba71a33661f
Jingyi Quuu <quj13@mcmaster.ca>
2020-03-30 13:39:18 -0400

    small message about the change

---
+++ assignment_1.py
@@ -0,0 +1,9 @@
+>>> print("hello world")
+hello world
+>>> hello world
+SyntaxError: invalid syntax
+>>> print("hello world"*3)
+hello worldhello worldhello world
+>>> print("hello world!"*3)
+hello world!hello world!hello world!
+>>> 
---
+++ firstgame.py
@@ -0,0 +1,9 @@
+print('first game')
+temp=input("guess a number:")
+guess=int(temp)
+if guess==8:
+    print ("ahh")
+    print ("That's it!")
+else:
+    print ("wrong! it's 8!")
+print ("game over!")
